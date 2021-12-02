b0a3e9710c6773717872bd1e65ba0f029de77f1b
Patrick <patadamrich@gmail.com>
2021-12-03 08:01:56 +1000

    init (duplicate from private repo)

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Portfolio
-My Personal Portfolio
+Thank you for checking out my personal portfolio.
+
+## How its made
+This portfolio is hosted on Netlify with a custom domain at <a href='https://patadamrich.com'>https://patadamrich.com/</a>.
+The site is constructed using: 
+- HTML
+- SCSS
+- JavaScript.
+
+This project was built in late 2021 as a place to showcase my various works. It has undergone many iterations throughout its small lifetime.
+
+### What I Learnt
+- Optimisation, Images take up a lot of time and space that can be easily reduced with compression and sizing.
+- Performance, Up until now I had never really thought about performance much but I realised that if I want to show off the best of my abilities I need to do so in the most optimised way. So I spent time reducing images sizes, following guides and advice from tests like 'lighthouse' and browsing the DevTools for new ways to improve the site.
+- Host, The distribution of a website from various hosts can greatly impact on the end-users performance and time-load. Selecting a host is difficult on a non-existant budget.
+
+## To Do (improvements)
+- Add Navigation support to mobile version
+
+## Site Status
+[![Netlify Status](https://api.netlify.com/api/v1/badges/abf25a98-6f7c-4774-90f7-852a20f06dae/deploy-status)](https://app.netlify.com/sites/patadam/deploys)
---
+++ static/javascript/main.js
@@ -0,0 +1,181 @@
+// GSAP Registration
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
+
+// Variables
+const projects = document.querySelectorAll(".project-li");
+const cta = document.querySelector("#hero-cta");
+
+// CTA scroll button on the landing page scrolls down 100vh to the main content.
+cta.addEventListener("click", () => {
+  document.querySelector("main").scrollIntoView();
+});
+
+function percentToPixel(element, percentage) {
+  let Width = element.offsetWidth;
+  let DistanceFromSide = element.offsetLeft;
+  return Width + DistanceFromSide;
+}
+
+// Projects animating into the page with scroll.
+let timeline = gsap
+  .timeline({
+    scrollTrigger: {
+      scrub: true,
+      trigger: projects[0],
+    },
+    defaults: { duration: 2, delay: 0.6 },
+  })
+
+  //.fromTo(projects[0], { x: -1 * percentToPixel(projects[0]), opacity: 0.6 }, { x: 0, opacity: 1 })
+  //.fromTo(projects[1], { x: percentToPixel(projects[1]), opacity: 0.6 }, { x: 0, opacity: 1 })
+  //.fromTo(projects[2], { x: -1 * percentToPixel(projects[2]), opacity: 0.6 }, { x: 0, opacity: 1 });
+
+  .fromTo(projects[0], { x: -80, opacity: 0.2 }, { x: 0, opacity: 1 })
+  .fromTo(projects[1], { x: 80, opacity: 0.2 }, { x: 0, opacity: 1 })
+  .fromTo(projects[2], { x: -80, opacity: 0.2 }, { x: 0, opacity: 1 });
+
+// Toggles the Theme of the web page
+
+const toggle = document.querySelector("#theme-toggle");
+const HTML = document.querySelector("html");
+const Icon = document.querySelector("#toggle-icon");
+
+// changes the html attribute data-theme when clicked.
+function toggleTheme() {
+  let currentState = HTML.getAttribute("data-theme"); // finds the current theme of the page
+  currentState == "dark" ? (newstate = "light") : (newstate = "dark"); // toggles the theme betwene ligth and dark
+  HTML.setAttribute("data-theme", newstate); // applies the new theme to the html attribute
+  Icon.innerHTML = newstate + "_mode"; // updates the icon displayed on the page
+  toggle.setAttribute("checked", newstate); // updates the checkbox
+  return newstate; // returns the new theme of the page to the calling instnace.
+}
+
+toggle.addEventListener("click", () => {
+  let theme = toggleTheme();
+  document.cookie = `data-theme=${theme}; path=/`; // sets the cookie when the theme is changed.
+});
+
+(function defaultSetup() {
+  // collects the cookie from cache and changes the default light theme to dark if theme is dark.
+  document.cookie.split(";").includes("data-theme=dark") ? toggleTheme() : "light";
+})();
+
+const mobileNav = document.querySelector(".mobile-nav-wrapper");
+const mobileNavClose = document.querySelector("#mobile-nav-control");
+mobileNavClose.addEventListener("click", () => {
+  if (mobileNav.classList.contains("hidden")) {
+    mobileNav.classList.remove("hidden");
+    mobileNavClose.innerHTML = "close";
+  } else {
+    mobileNav.classList.add("hidden");
+    mobileNavClose.innerHTML = "menu";
+  }
+});
+
+const links = mobileNav.querySelectorAll("li a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileNav.classList.add("hidden");
+    mobileNavClose.innerHTML = "menu";
+  });
+});
+
+const db = [
+  {
+    title: "Coming Soon",
+    subtitle: "Resturant Website Design",
+    type: "Adobe XD Design",
+    description: "...",
+    link_live: "https://patadamrich.com",
+    link_source: "https://github.com",
+    img: "/static/images/cafe.webp",
+  },
+  {
+    title: "Tasty Roads",
+    subtitle: "Resturant Website",
+    type: "Static Website",
+    description:
+      "Tasty Roads is a website creation based off the local business 'tasty roads'. This is a mock-up site and not a production site used by the resturant. Hosted on Netlify.",
+    link_live: "https://tastyroads.patadamrich.com",
+    link_source: "https://github.com",
+    img: "/static/images/restaurant.webp",
+  },
+  {
+    title: "Coming Soon",
+    subtitle: "Gym & Fitness Club",
+    type: "Website with CMS",
+    description: "...",
+    link_live: "https://reps.patadamrich.com",
+    link_source: "https://github.com",
+    img: "/static/images/gym.webp",
+  },
+];
+
+const popup = document.querySelector(".card-wrapper");
+const cardClose = popup.querySelector(".card-close");
+const extraProjects = document.querySelectorAll(".extra-project-li");
+extraProjects.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    popup.classList.remove("hidden");
+    setValue(index);
+  });
+});
+
+cardClose.addEventListener("click", () => {
+  popup.classList.add("hidden");
+});
+
+const backButton = popup.querySelector(".back");
+backButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let currentIndex = backButton.getAttribute("current");
+  if (currentIndex > 0) {
+    setValue(currentIndex - 1);
+  }
+});
+
+const forwardButton = popup.querySelector(".forward");
+forwardButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let currentIndex = forwardButton.getAttribute("current");
+  if (currentIndex < 2) {
+    setValue(parseInt(currentIndex) + 1);
+  }
+});
+
+const dots = popup.querySelectorAll(".dot");
+const cardNav = popup.querySelectorAll(".card-nav li");
+
+function setValue(index) {
+  let content = popup.querySelector(".card-content");
+  // Set content
+  content.querySelector(".title").innerHTML = db[index].title;
+  content.querySelector(".sub-title").innerHTML = db[index].subtitle;
+  content.querySelector(".description").innerHTML = db[index].type;
+  content.querySelector(".text").innerHTML = db[index].description;
+  popup.querySelector(".image").setAttribute("src", db[index].img);
+  // links
+  content.querySelector(".live").setAttribute("href", db[index].link_live);
+  content.querySelector(".source").setAttribute("href", db[index].link_source);
+  // nav
+  content.querySelector(".back").setAttribute("current", index);
+  content.querySelector(".forward").setAttribute("current", index);
+  // dots & numbers
+  dots.forEach((element, i) => {
+    if (i == index) {
+      element.classList.add("bg-blue");
+    } else if (element.classList.contains("bg-blue")) {
+      element.classList.remove("bg-blue");
+    }
+  });
+  // numbers
+  cardNav.forEach((ele, ind) => {
+    console.log(ele);
+    if (ind == index) {
+      ele.classList.add("blue");
+    } else if (ele.classList.contains("blue")) {
+      ele.classList.remove("blue");
+    }
+  });
+}
